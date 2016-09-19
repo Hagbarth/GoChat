5bea67899c6c49c08d25a2424e033901bc55e295
hagbarth <emil@hivebeat.com>
2016-09-19 22:44:17 +0200

    convert to spaces...

--- app/chat/types.go
+++ app/chat/types.go
@@ -1,28 +1,28 @@
 package chat
 
 import (
-	"github.com/satori/go.uuid"
-	"time"
+  "github.com/satori/go.uuid"
+  "time"
 )
 
 type MessageRequest struct {
-	Uid     string
-	Message string
+  Uid     string
+  Message string
 }
 
 type User struct {
-	ID       uuid.UUID
-	Nickname string
+  ID       uuid.UUID
+  Nickname string
 }
 
 type Message struct {
-	ID        uuid.UUID
-	Timestamp time.Time
-	Message   string
-	User      *User
+  ID        uuid.UUID
+  Timestamp time.Time
+  Message   string
+  User      *User
 }
 
 type MessageBoard struct {
-	Messages []Message
-	Users    []User
+  Messages []Message
+  Users    []User
 }
--- app/server.go
+++ app/server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/hagbarth/GoChat/app/chat"
-	"net/http"
+  "encoding/json"
+  "github.com/hagbarth/GoChat/app/chat"
+  "net/http"
 )
 
 var messageBoard chat.MessageBoard
@@ -14,65 +14,65 @@ var messageBoard chat.MessageBoard
 * @param w {http.ResponseWriter}
 **/
 func respondJSON(object interface{}, w http.ResponseWriter) {
-	js, err := json.Marshal(object)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+  js, err := json.Marshal(object)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
 }
 
 /**
 * Adds a new user and responds with the user id
 **/
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var u chat.User
-	err := decoder.Decode(&u)
-	if err != nil {
-		panic("oh no!")
-	}
-	respondJSON(messageBoard.AddUser(u), w)
+  decoder := json.NewDecoder(r.Body)
+  var u chat.User
+  err := decoder.Decode(&u)
+  if err != nil {
+    panic("oh no!")
+  }
+  respondJSON(messageBoard.AddUser(u), w)
 }
 
 /**
 * Handles message requests
 **/
 func handleMessages(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		addNewMessage(w, r)
-		return
-	}
-	getMessages(w, r)
-	return
+  if r.Method == "POST" {
+    addNewMessage(w, r)
+    return
+  }
+  getMessages(w, r)
+  return
 }
 
 /**
 * Adds a new message and returns all messages
 **/
 func addNewMessage(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var m chat.MessageRequest
-	err := decoder.Decode(&m)
-	if err != nil {
-		panic("oh no!")
-	}
-	messages := messageBoard.AddMessage(m.Uid, m.Message)
-	respondJSON(messages, w)
+  decoder := json.NewDecoder(r.Body)
+  var m chat.MessageRequest
+  err := decoder.Decode(&m)
+  if err != nil {
+    panic("oh no!")
+  }
+  messages := messageBoard.AddMessage(m.Uid, m.Message)
+  respondJSON(messages, w)
 }
 
 /**
 * Gets all messages
 **/
 func getMessages(w http.ResponseWriter, r *http.Request) {
-	respondJSON(messageBoard.Messages, w)
+  respondJSON(messageBoard.Messages, w)
 }
 
 func main() {
-	messageBoard = chat.NewMessageBoard()
-	http.Handle("/", http.FileServer(http.Dir("../static")))
-	http.HandleFunc("/login", handleLogin)
-	http.HandleFunc("/messages", handleMessages)
-	http.ListenAndServe(":8000", nil)
+  messageBoard = chat.NewMessageBoard()
+  http.Handle("/", http.FileServer(http.Dir("../static")))
+  http.HandleFunc("/login", handleLogin)
+  http.HandleFunc("/messages", handleMessages)
+  http.ListenAndServe(":8000", nil)
 }
